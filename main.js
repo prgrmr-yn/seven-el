20ad06d8dac99feff7bf2197465a2eb071909983
Yatin Nagpal <nnega4428@gmail.com>
2023-02-01 16:16:04 +1100

    updated html

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 const ulEl = document.querySelector('.responsive-table')
 console.log(ulEl);
-
-let li = '';
+ulEl.innerHTML = 'Loading.....  '
+let tableData = '';
 
 function func(data) {
   const allPrices = data['regions'][0]['prices']
   allPrices.forEach(p => {
-    li +=`<li class="table-row">
-              <div class="col col-1" data-label="">${p.state}</div>
-              <div class="col col-2" data-label="Customer Name">7-twelve ${p.suburb}</div>
-              <div class="col col-3" data-label="Amount">${p.price}</div>
-              <div class="col col-4" data-label="Payment Status">${p.type}</div>
-          </li>`
+    tableData +=`     <tr>
+    <td><strong>${p.suburb} ${p.state}</strong></td>
+    <td>${p.price}</td>
+    <td>${p.type} </td>
+  </tr>`
   });
 
-  ulEl.insertAdjacentHTML('beforeend', `${li}`);
+  ulEl.insertAdjacentHTML('beforeend', `${tableData}`);
 }
 
 let url = '';
@@ -29,6 +28,7 @@ fetch('https://projectzerothree.info/api.php?format=json')
   });
 
 setTimeout(() => {
+  ulEl.innerHTML = ''
   fetch(url)
   .then(response => response.json())
   .then(data => {
