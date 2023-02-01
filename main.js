0a70656c34525e2157fbd3a88ce68de3449837a4
Yatin Nagpal <nnega4428@gmail.com>
2023-02-01 15:33:41 +1100

    made a little change to extract data using redirected url

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 const ulEl = document.querySelector('.responsive-table')
 console.log(ulEl);
 
-li = '';
-function func(data) {
+let li = '';
 
+function func(data) {
   const allPrices = data['regions'][0]['prices']
   allPrices.forEach(p => {
-    li += `<li class="table-row">
-  <div class="col col-1" data-label="">${p.state}</div>
-  <div class="col col-2" data-label="Customer Name">7-twelve ${p.suburb}</div>
-  <div class="col col-3" data-label="Amount">${p.price}</div>
-  <div class="col col-4" data-label="Payment Status">${p.type}</div>
-  </li>`
+    li +=`<li class="table-row">
+              <div class="col col-1" data-label="">${p.state}</div>
+              <div class="col col-2" data-label="Customer Name">7-twelve ${p.suburb}</div>
+              <div class="col col-3" data-label="Amount">${p.price}</div>
+              <div class="col col-4" data-label="Payment Status">${p.type}</div>
+          </li>`
   });
 
   ulEl.insertAdjacentHTML('beforeend', `${li}`);
 }
 
+let url = '';
 
-fetch('https://projectzerothree.info/prices/1675063023.json')
+fetch('https://projectzerothree.info/api.php?format=json')
+  .then(response => {
+    url = response.url;
+    return url
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+setTimeout(() => {
+  fetch(url)
   .then(response => response.json())
   .then(data => {
     func(data)
@@ -26,3 +37,4 @@ fetch('https://projectzerothree.info/prices/1675063023.json')
   .catch(error => {
     console.error(error);
   });
+}, 800);
